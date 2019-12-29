77af49d443afa229c44107b2fcee44c0ba3aad27
tomasbal <omislav.balabanov@students.finki.ukim.mk>
2019-12-29 03:28:22 +0100

    Final

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,57 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password } = formData;
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    console.log('Sucess');
+  };
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign in</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Sign Into Your ACcount
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+            name='email'
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            required
+            minLength='6'
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't have an account? <Link to='/register'>Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+
+export default Login;
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+//connect Database
+connectDB();
+
+//Init Middleware
+
+app.use(express.json({
+    extended: false
+}));
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('API Running'));
 
+//define routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
+    console.log(`Server started on ${PORT}`);
+});
+
+process.on('SIGINT', () => {
+    console.log("Exiting server!");
+    process.exit();
 });
